ebe13456e26c62390fcae86370a8c4108c35f0a8
William Glodek <wglodek@breakpoint-labs.com>
2016-10-23 20:19:19 -0400

    adding tests for dfile.py

---
+++ tests/dfile_test.py
@@ -0,0 +1,19 @@
+import pytest
+from dshell import dfile
+
+
+@pytest.fixture(scope='module')
+def data_file_obj():
+    return dfile.dfile(data='AAAA')
+
+
+def test_dfile_initializations(data_file_obj):
+    print type(data_file_obj)
+    print dir(data_file_obj)
+    assert data_file_obj is not None
+
+
+def test_dfile_local_filename_generation(data_file_obj):
+    orig = 'file:v2'
+    fname = data_file_obj.generate_local_filename(orig)
+    assert fname.endswith('file_v2')
