35d620fdb77e54da54ade6edb01fb98c2d1eda50
William Glodek <wglodek@breakpoint-labs.com>
2016-11-11 16:58:03 -0500

    adding more exceptions

--- dshell/exceptions.py
+++ dshell/exceptions.py
@@ -1,2 +1,10 @@
 class DshellException(Exception):
     '''generic Dshell exception'''
+
+
+class MissingDecoder(DshellException):
+    '''missing decoder'''
+
+
+class InvalidDecoderOption(DshellException):
+    '''optiondict error in the decoder'''
